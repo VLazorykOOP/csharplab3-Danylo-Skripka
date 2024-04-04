f9f0afd6c2fd989d105306b36063fcf489f6768f
Danylo-Skripka <125260561+Danylo-Skripka@users.noreply.github.com>
2024-04-04 09:31:21 +0300

    Add files via upload

---
+++ lab3C#1/lab3C#1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Money
+{
+    // Поля
+    protected int nominal;
+    protected int num;
+
+    // Конструктор
+    public Money(int nominal, int num)
+    {
+        this.nominal = nominal;
+        this.num = num;
+    }
+
+    // Методи
+    public void PrintMoney()
+    {
+        Console.WriteLine($"Номінал: {nominal}, Кількість купюр: {num}");
+    }
+
+    public bool CanBuy(double cost)
+    {
+        return cost <= TotalAmount();
+    }
+
+    public int CountItems(double cost)
+    {
+        return (int)(TotalAmount() / cost);
+    }
+
+    // Властивості
+    public int Nominal
+    {
+        get { return nominal; }
+        set { nominal = value; }
+    }
+
+    public int Num
+    {
+        get { return num; }
+        set { num = value; }
+    }
+
+    public double TotalAmount()
+    {
+        return nominal * num;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Приклад використання класу Money
+        Money wallet = new Money(100, 5); // Гаманець з 5 купюрами по 100 гривень кожна
+        wallet.PrintMoney(); // Виводимо інформацію про гаманець
+
+        double itemCost = 250; // Ціна товару
+        int itemCount = wallet.CountItems(itemCost); // Визначаємо кількість товару, яку можна купити
+        bool canBuy = wallet.CanBuy(itemCost); // Перевіряємо, чи можна купити товар
+
+        if (canBuy)
+        {
+            Console.WriteLine($"Можна купити {itemCount} товару(-ів) за {itemCost} грн.");
+        }
+        else
+        {
+            Console.WriteLine("Недостатньо коштів для покупки товару.");
+        }
+    }
+}
---
+++ lab3C#2/lab3C#2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+class Ship
+{
+    protected string name;
+    protected double length;
+    protected double width;
+
+    public Ship(string name, double length, double width)
+    {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+    }
+
+    public virtual void Show()
+    {
+        Console.WriteLine($"Назва: {name}, Довжина: {length}, Ширина: {width}");
+    }
+}
+
+class Steamship : Ship
+{
+    protected double enginePower;
+
+    public Steamship(string name, double length, double width, double enginePower) : base(name, length, width)
+    {
+        this.enginePower = enginePower;
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Потужність двигуна: {enginePower}");
+    }
+}
+
+class Sailboat : Ship
+{
+    protected int sailCount;
+
+    public Sailboat(string name, double length, double width, int sailCount) : base(name, length, width)
+    {
+        this.sailCount = sailCount;
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Кількість вітрил: {sailCount}");
+    }
+}
+
+class Corvette : Ship
+{
+    protected int gunCount;
+
+    public Corvette(string name, double length, double width, int gunCount) : base(name, length, width)
+    {
+        this.gunCount = gunCount;
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Кількість гармат: {gunCount}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Ship[] fleet = new Ship[4];
+        fleet[0] = new Steamship("Титанік", 269, 28, 20000);
+        fleet[1] = new Sailboat("Бригантина", 55, 12, 3);
+        fleet[2] = new Corvette("Адмірал", 64, 10, 20);
+        fleet[3] = new Steamship("Океанія", 320, 32, 25000);
+
+        Console.WriteLine("Список суден:");
+        foreach (Ship ship in fleet)
+        {
+            ship.Show();
+            Console.WriteLine();
+        }
+    }
+}
